960c6a4edfcf5c701918f75413e272bdb02ed25b
Gam <gavlevi@uab.edu>
2025-01-05 16:36:07 -0600

    Updated Encapsulation Code

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ public class Main {
     public static void main(String[] args) {
         //No constructor means Java automatically creates one; since variables are public, they
         //can be created here
-        Player player1 = new Player();
-        player1.name = "Gam";
-        player1.health = 34;
-        player1.weapon = "Diamond Axe";
-
-        int damage = 10;
-        player1.loseHealth(damage);
-        System.out.println("Remaining health: " + player1.healthRemaining());
-
-        player1.loseHealth(30);
-        player1.health = 24;
-        System.out.println("Remaining health: " + player1.healthRemaining());
+//        Player player1 = new Player();
+//        player1.name = "Gam";
+//        player1.health = 34;
+//        player1.weapon = "Diamond Axe";
+//
+//        int damage = 10;
+//        player1.loseHealth(damage);
+//        System.out.println("Remaining health: " + player1.healthRemaining());
+//
+//        player1.loseHealth(30);
+//        player1.health = 24;
+//        System.out.println("Remaining health: " + player1.healthRemaining());
+        EnhancedPlayer gam = new EnhancedPlayer("Gam");
+        System.out.println("Initial health is " + gam.healthRemaining());
     }
 }
 
